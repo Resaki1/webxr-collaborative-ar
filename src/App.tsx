c8245d23fe86a0c999314dc99ba39d858b34de67
Resaki1 <christian.reski@t-online.de>
2021-11-19 17:36:39 +0100

    added ability to add anchors

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import type {
+  XRAnchor,
   XRFrame,
+  XRHitTestResult,
   XRHitTestSource,
   XRPose,
   XRRay,
@@ -21,8 +23,12 @@ import { Renderer, createWebGLContext } from "./renderer/core/renderer.js";
 import WebXRButton from "./components/WebXRButton";
 import { getOrientationAbs, multiply } from "./ts/vectors";
 import { Gltf2Node } from "./renderer/nodes/gltf2";
+
+// import GLTFs so that snowpack includes them during compilation
 // @ts-ignore
-import * as reticleTest from "./media/gltf/reticle/reticle.gltf";
+import * as reticleGltf from "./media/gltf/reticle/reticle.gltf";
+// @ts-ignore
+import * as sunflowerGltf from "./media/gltf/sunflower/sunflower.gltf";
 
 interface AppProps {}
 
@@ -33,6 +39,8 @@ function App({}: AppProps) {
   let xrHitTestSource: XRHitTestSource;
   let xrPose: XRPose | null;
   let xrDepth: number | null;
+  let reticleHitTestResult: XRHitTestResult | null;
+  let anchoredObjects: { anchoredObject: Gltf2Node; anchor: XRAnchor }[] = [];
 
   let reticle = new Gltf2Node({
     url: "./dist/media/gltf/reticle/reticle.gltf",
@@ -46,6 +54,34 @@ function App({}: AppProps) {
   const sensor = new AbsoluteOrientationSensor(options);
 
   const xr = (navigator as any)?.xr as XRSystem;
+
+  const addAnchoredObjectsToScene = (anchor: XRAnchor) => {
+    const flower = new Gltf2Node({
+      url: "./dist/media/gltf/sunflower/sunflower.gltf",
+    });
+    scene.addNode(flower);
+    anchoredObjects.push({
+      anchoredObject: flower,
+      anchor: anchor,
+    });
+  };
+
+  const onSelected = () => {
+    if (reticle.visible) {
+      // TODO: differentiate between calibration and "play" phase
+      if (reticleHitTestResult) {
+        // create an anchor
+        // @ts-ignore
+        reticleHitTestResult
+          // @ts-ignore
+          .createAnchor()
+          .then((anchor) => {
+            addAnchoredObjectsToScene(anchor);
+          })
+          .catch((error) => console.log(`Could not create anchor: ${error}`));
+      }
+    }
+  };
 
   const initXR = async () => {
     sensor.start();
@@ -63,7 +99,7 @@ function App({}: AppProps) {
   };
 
   const onSessionStarted = (session: XRSession) => {
-    //session.addEventListener("select", onSelected);
+    session.addEventListener("select", onSelected);
 
     const gl = createWebGLContext({ xrCompatible: true });
     if (gl) {
@@ -117,6 +153,7 @@ function App({}: AppProps) {
 
       // TODO: Show reticle only using depth data while calibrating
       // TODO: Only use depth data while calibrating
+      xrDepth = null;
       if (xrDepth) {
         // add depth data to improve reticle accuracy
         // @ts-ignore
@@ -129,19 +166,30 @@ function App({}: AppProps) {
         let rayPositionVector = [ray.origin.x, ray.origin.y, ray.origin.z];
         let hitTestVector = multiply(rayDirectionVector, xrDepth);
 
+        reticle.visible = true;
         reticle.translation = rayPositionVector.map(
           (value, index) => value + hitTestVector[index],
         );
       } else if (xrHitTestSource) {
-        let hitTestResult = frame.getHitTestResults(xrHitTestSource);
+        let hitTestResults = frame.getHitTestResults(xrHitTestSource);
 
-        if (hitTestResult.length > 0) {
-          let pose = hitTestResult[0].getPose(xrRefSpace);
+        if (hitTestResults.length > 0) {
+          let pose = hitTestResults[0].getPose(xrRefSpace);
+          reticleHitTestResult = hitTestResults[0];
           reticle.visible = true;
 
           reticle.matrix = pose?.transform.matrix;
         } else reticle.visible = false;
       } else reticle.visible = false;
+    }
+
+    console.log(anchoredObjects);
+    for (const { anchoredObject, anchor } of anchoredObjects) {
+      // only update the object's position if it's still in the list
+      // of frame.trackedAnchors
+      if (!frame.trackedAnchors?.has(anchor)) continue;
+      const anchorPose = frame.getPose(anchor.anchorSpace, xrRefSpace);
+      if (anchorPose) anchoredObject.matrix = anchorPose.transform.matrix;
     }
 
     scene.startFrame();
